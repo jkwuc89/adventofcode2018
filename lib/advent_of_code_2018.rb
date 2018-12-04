522dd604bb50bb0f3901a53b41e3f11789315dea
Keith Wedinger <keith.wedinger@joinroot.com>
2018-12-03 20:43:21 -0500

    Completed day 1 puzzle 2

--- lib/advent_of_code_2018.rb
+++ lib/advent_of_code_2018.rb
@@ -1,13 +1,37 @@
 require "adventofcode2018/version"
+require "ostruct"
+require "set"
 
 module AdventOfCode2018
-  def self.day1_puzzle1(adjustments)
-    frequency = 0
+  def self.day1_puzzle1(starting_frequency: 0, adjustments:)
+    frequency = starting_frequency
     adjustments.each do |adjustment|
-      operator = adjustment[0]
-      operand = adjustment[1..-1].to_i
-      frequency += (operator == "+" ? operand : -operand)
+      adj = _day1_deconstruct_adjustment(adjustment)
+      frequency += (adj.operator == "+" ? adj.operand : -adj.operand)
     end
     frequency
+  end
+
+  def self.day1_puzzle2(adjustments)
+    current_frequency = 0
+    frequencies = Set[current_frequency]
+    loop do
+      adjustments.each do |adjustment|
+        adj = _day1_deconstruct_adjustment(adjustment)
+        current_frequency += (adj.operator == "+" ? adj.operand : -adj.operand)
+        return current_frequency if frequencies.include?(current_frequency)
+
+        frequencies.add(current_frequency)
+      end
+    end
+  end
+
+  private
+
+  def self._day1_deconstruct_adjustment(adjustment)
+    deconstructed = OpenStruct.new
+    deconstructed.operator = adjustment[0]
+    deconstructed.operand = adjustment[1..-1].to_i
+    deconstructed
   end
 end
--- spec/advent_of_code2018_spec.rb
+++ spec/advent_of_code2018_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe AdventOfCode2018 do
   context "Day 1: https://adventofcode.com/2018/day/1" do
     context "Puzzle 1: Adjust frequency" do
       context "Examples" do
-        it { expect(described_class.day1_puzzle1(%w(+1 +1 +1))).to eq(3) }
-        it { expect(described_class.day1_puzzle1(%w(+1, +1, -2))).to eq(0) }
-        it { expect(described_class.day1_puzzle1(%w(-1, -2, -3))).to eq(-6) }
+        it { expect(described_class.day1_puzzle1(:adjustments => %w[+1 +1 +1])).to eq(3) }
+        it { expect(described_class.day1_puzzle1(:adjustments => %w[+1 +1 -2])).to eq(0) }
+        it { expect(described_class.day1_puzzle1(:adjustments => %w[-1 -2 -3])).to eq(-6) }
       end
       context "Puzzle input" do
-        it { expect(described_class.day1_puzzle1(IO.readlines("#{TEST_RESOURCE_DIR}/day1puzzle1input.txt"))).to eq(525) }
+        it { expect(described_class.day1_puzzle1(:adjustments => IO.readlines("#{TEST_RESOURCE_DIR}/day1input.txt"))).to eq(525) }
+      end
+    end
+
+    context "Puzzle 2: Find duplicate frequency" do
+      context "Examples" do
+        it { expect(described_class.day1_puzzle2(%w[+1 -1])).to eq(0) }
+        it { expect(described_class.day1_puzzle2(%w[+3 +3 +4 -2 -4])).to eq(10) }
+        it { expect(described_class.day1_puzzle2(%w[-6 +3 +8 +5 -6])).to eq(5) }
+        it { expect(described_class.day1_puzzle2(%w[+7 +7 -2 -7 -4])).to eq(14) }
+      end
+
+      context "Puzzle input" do
+        it { expect(described_class.day1_puzzle2(IO.readlines("#{TEST_RESOURCE_DIR}/day1input.txt"))).to eq(75749) }
       end
     end
   end
